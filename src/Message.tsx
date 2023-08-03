136eb7bd83264d6ac1f8f5f93e15e0d5c5dca9e7
Billie Xu <billie.xu@bp.com>
2023-08-03 17:31:46 +0100

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+    // JavaScript XML
+    const name : string = "Billie";
+
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function ListGroup() {
+  let items = ["New York", "London", "Paris", "San Francesco", "Tokyo"];
+
+  //Hook
+  const [selectedIndex, updateSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No Items Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              updateSelectedIndex(index);
+            }}
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
